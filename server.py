ad958f9c1e588fd229845f7aa6f9a6a9d0555fdc
Josef Rousek <josef.rousek@gmail.com>
2019-04-27 16:24:54 +0200

    Improve search results

--- server.py
+++ server.py
@@ -22,11 +22,13 @@ def geocode(location):
 
 def zomato_api_call(path):
     header = {
-        "Accept": "application/json", "user_key": os.environ.get("ZOMATO_API_KEY")
+        "Accept": "application/json",
+        "user_key": os.environ.get("ZOMATO_API_KEY"),
     }
     response = requests.get(path, headers=header)
     response.raise_for_status()
     return response.json()
+
 
 def format_dish(dish):
     dish_price = dish["dish"]["price"]
@@ -34,8 +36,10 @@ def format_dish(dish):
         dish_price = f"*{dish_price}*"
     return dish["dish"]["name"] + " " + dish_price
 
+
 def try_get_daily_menu(restaurant):
     restaurant_id = restaurant["restaurant"]["id"]
+    print(f"looking for " + restaurant["restaurant"]["name"])
     try:
         menu = zomato_api_call(
             f"https://developers.zomato.com/api/v2.1/dailymenu?res_id={restaurant_id}"
@@ -43,13 +47,10 @@ def try_get_daily_menu(restaurant):
         if menu["daily_menus"]:
             menu_data = menu["daily_menus"][0]["daily_menu"]
             restaurant_name = restaurant["restaurant"]["name"]
-            menu_date = datetime.datetime.fromisoformat(menu_data["start_date"]).strftime("%A, %d. %B")
-            dishes = "\n".join(
-                [
-                    format_dish(dish)
-                    for dish in menu_data["dishes"]
-                ]
-            )
+            menu_date = datetime.datetime.fromisoformat(
+                menu_data["start_date"]
+            ).strftime("%A, %d. %B")
+            dishes = "\n".join([format_dish(dish) for dish in menu_data["dishes"]])
             return f"*{restaurant_name}*\n{menu_date}\n\n{dishes}"
     except Exception as error:
         print(error)
@@ -58,16 +59,15 @@ def try_get_daily_menu(restaurant):
 @background.task
 def get_restaurants(location, lat, lng, response_url):
     restaurants = zomato_api_call(
-        f"https://developers.zomato.com/api/v2.1/geocode?lat={lat}&lon={lng}"
+        f"https://developers.zomato.com/api/v2.1/search?lat={lat}&lon={lng}&radius=500&sort=real_distance"
     )
 
     daily_menus = [
-        try_get_daily_menu(restaurant)
-        for restaurant in restaurants["nearby_restaurants"]
+        try_get_daily_menu(restaurant) for restaurant in restaurants["restaurants"]
     ]
 
     text = "\n\n".join([menu for menu in daily_menus if menu])
-    print(text)
+    text = text or "Nothing found"
     response = requests.post(
         response_url, json={"response_type": "in_channel", "text": text, "mrkdwn": True}
     )
